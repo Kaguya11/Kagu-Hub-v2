c2299a8c080d6eae3416b1917e95e2e61bc8a0e8
Kaguya11 <106523037+Kaguya11@users.noreply.github.com>
2023-01-01 21:27:31 +0500

    Update KaguHub.lua

--- KaguHub.lua
+++ KaguHub.lua
@@ -69,31 +69,9 @@ Tab:AddButton({
 Tab:AddButton({
 	Name = "Esp MM2(PC and Mobile)",
 	Callback = function()
-		local function getRoleColor(plr)
-			if (plr.Backpack:FindFirstChild("Knife") or plr.Character:FindFirstChild("Knife")) then
-				return Color3.new(255, 0, 0)
-			elseif (plr.Backpack:FindFirstChild("Gun") or plr.Character:FindFirstChild("Gun")) then
-				return Color3.new(0, 0, 255)
-			else
-				return Color3.new(0, 255, 0)
-			end
-		 end
-		 
-		 while true do
-			for _, v in pairs(game.Players:GetChildren()) do
-				if v ~= game.Players.LocalPlayer and v.Character and not v.Character:FindFirstChild("Highlight") then
-					Instance.new("Highlight", v.Character)
-					v.Character.Highlight.FillTransparency = 0.5
-					v.Character.Highlight.OutlineTransparency = 0.5
-					v.Character.Highlight.FillColor = getRoleColor(v)
-				elseif (v ~= game.Players.LocalPlayer and v.Character and v.Character:FindFirstChild("Highlight")) then
-					v.Character.Highlight.FillColor = getRoleColor(v)
-				end
-			end
-			wait(0.1)
-		 end
-  	end    
-})
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaguya11/esp-script-mm2/main/espmm2.lua"))()
+])   
+
 
 Tab:AddButton({
 	Name = "Viribus(PC and Mobile)",
